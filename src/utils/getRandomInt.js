014545458cfe2ad65a0822d097f4792416254242
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-01-20 16:19:48 +0800

    Fix merge conflict

---
+++ src/utils/getRandomInt.js
@@ -0,0 +1,6 @@
+// getRandomInt(min: number, max: number): number
+export default function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
