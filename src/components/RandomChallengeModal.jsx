5f13871692cb5e6d43166200e4a907606910736c
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-01-21 03:09:34 +0800

    Test whether API key is accessible

--- src/components/RandomChallengeModal.jsx
+++ src/components/RandomChallengeModal.jsx
@@ -106,7 +106,7 @@ export default function RandomChallengeModal({
   }, [randomFactOpen, opened, challenges.length]);
 
   console.log(challenges[index], index)
-  console.log("API key", import.meta.env.VITE_YOUTUBE_API_KEY)
+  console.log("API key:", import.meta.env.VITE_YOUTUBE_API_KEY)
   return (
     <Modal
       opened={opened}
