2ecc456b6a71af87a754341b03934d6e9cda8edb
prolude1 <79588462+prolude1@users.noreply.github.com>
2024-01-20 22:07:17 +0800

    Simplified the search params

--- src/components/challenges/URL_youtube.jsx
+++ src/components/challenges/URL_youtube.jsx
@@ -3,7 +3,7 @@ import { Button } from "@mantine/core";
 import { useState } from "react";
 
 export default function Check_URL_youtube({ onCorrectAnswer, onIncorrectAnswer }) {
-    const [inputURL, setURL] = useState('https://www.youtube.com/watch?v=gWnOpUqTzBg');
+    const [inputURL, setURL] = useState('');
     let todaysDate = new Date()
     let youtubeCreationDate = new Date(2010, 1, 1)
 
@@ -11,27 +11,29 @@ export default function Check_URL_youtube({ onCorrectAnswer, onIncorrectAnswer }
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        const TempVideoKey = inputURL.split("v=")[1]
-        let VideoKey = TempVideoKey
-        try {
-            VideoKey = TempVideoKey.split("&")[0]
+        if (inputURL.length === 0) {
+            onIncorrectAnswer();
+            return;
         }
-        catch (error) {
-            console.error(error)
+
+        const videoSearchParams = inputURL.split("?")[1]
+
+        if (!videoSearchParams) {
+            return onIncorrectAnswer();
         }
+
+        const videoKey = new URLSearchParams(videoSearchParams).get("v")
+        console.log(videoKey)
 
         const APIKey = import.meta.env.VITE_YOUTUBE_API_KEY
         const URL_check = "https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=$vidkey&key=$apikey&part=snippet"
-        let URL_check_video = URL_check.replace("$vidkey", VideoKey)
+        let URL_check_video = URL_check.replace("$vidkey", videoKey)
         let URL_check_video_api = URL_check_video.replace("$apikey", APIKey)
         let data_youtube = (await fetch(URL_check_video_api));
         let video_date_published = await data_youtube.json()
         video_date_published = (video_date_published["items"][0]["snippet"]["publishedAt"]).split("T")[0]
         video_date_published = new Date(video_date_published)
-        console.log("Random date")
-        console.log(targetDate);
-        console.log("User's input")
-        console.log(video_date_published)
+        
         if (targetDate.getDate() == video_date_published.getDate() && targetDate.getMonth() == video_date_published.getMonth()
             && targetDate.getFullYear() == video_date_published.getFullYear()) {
             onCorrectAnswer()
@@ -52,6 +54,7 @@ export default function Check_URL_youtube({ onCorrectAnswer, onIncorrectAnswer }
             <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: 16 }}>
                 <TextInput
                     value={inputURL}
+                    placeholder='https://www.youtube.com/watch?v=gWnOpUqTzBg'
                     onChange={(event) => setURL(event.currentTarget.value)}
                 />
                 <Button type="submit">Submit</Button>
