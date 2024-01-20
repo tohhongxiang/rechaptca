61874b9dd1ab22b2c7e9eb7e04feef8dde7a4557
Toh Hong Xiang <tohhongxiang@gmail.com>
2024-01-20 14:21:01 +0800

    Add modal

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,20 @@
-import { Checkbox } from "@mantine/core";
+import { Checkbox, Modal } from "@mantine/core";
+import { useDisclosure } from '@mantine/hooks';
+
 
 export default function Game() {
+    const [opened, { open, close }] = useDisclosure(false);
     return (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "100vh" }}>
-            <Checkbox label="Click here to verify if you're a human" />
-        </div>
+        <>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100vw", height: "100vh" }}>
+                <Checkbox label="Click here to verify if you're a human" onClick={open} />
+            </div>
+            <Modal opened={opened} onClose={close} title="Captcha Challenge" centered>
+                <div>
+                    <h1>Are you a real human?</h1>
+                </div>
+            </Modal>
+        </>
+
     )
 }
